4de16b95821a09ce731fc5ab93b1f27e72b7feb9
Pierre Dubois <info@pdprogrammeur.com>
2012-07-09 06:47:38 -0500

    small update

--- src/js/workers/pffd.js
+++ src/js/workers/pffd.js
@@ -1,5 +1,6 @@
 /*!
  * Form enhancement project - Up To v1.0a1 / Amélioration des formulaire - Jusqu'à v1.0a6
+ * Progressive Form Field Disclosure
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * Terms and conditions of use: http://tbs-sct.ircan.gc.ca/projects/gcwwwtemplates/wiki/Terms
  * Conditions régissant l'utilisation : http://tbs-sct.ircan.gc.ca/projects/gcwwwtemplates/wiki/Conditions
