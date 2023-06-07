fb5a083ba360c3c3a8af10f49aab52698884f18c
Pierre Dubois <duboisp2@gmail.com>
2023-06-07 13:11:07 -0400

    data json conditional - Working prototype 2

--- src/plugins/wb-data-json/data-json.js
+++ src/plugins/wb-data-json/data-json.js
@@ -361,12 +361,13 @@ var componentName = "wb-data-json",
 			if ( !clone && useClone ) {
 
 				// ( !clone && useClone ) => this is a Grouping template
-				console.log( "Create template in dataIterator" );
+				/*console.log( "Create template in dataIterator" );
 				console.log( elm );
 				console.log( content );
 				console.log( mappingConfig );
 				console.log( useClone );
 				console.log( "__" ) ;
+				*/
 
 				clone = useClone;
 				/*
@@ -387,16 +388,17 @@ var componentName = "wb-data-json",
 			}
 
 
-
+/*
 			if ( !clone || clone.nodeType === 11 ) {
 				console.log( "Empty clone...");
 				console.log( useClone );
 				console.log( clone );
 				console.log( elm );
 				console.log( mappingConfig );
-			}
+			}*/
 
-			processConditional( elm, clone, i_cache, mappingConfig );
+			// Not needed anymore, it is check before to initiate the mapping
+			// processConditional( elm, clone, i_cache, mappingConfig );
 
 
 
@@ -420,6 +422,7 @@ var componentName = "wb-data-json",
 			if ( !cloneArray ) {
 				cloneArray = [];
 			}*/
+
 
 
 
@@ -473,7 +476,7 @@ var componentName = "wb-data-json",
 		}
 
 	},
-
+/*
 	processConditional = function( elm, clone, content, mappingConfig, behavioural ) {
 
 		var conditions = mappingConfig.conditions,
@@ -541,15 +544,85 @@ var componentName = "wb-data-json",
 		return cloneArray;
 
 
+	},*/
+
+	typeMappingIterator = function() {
+
+		// bag
+
+		// for loop here and evaluate each of them, once success, trigger the process mapping.
+
 	},
 
-	GlobalIsArrayTrue = false,
+	canProcessMapping = function( content, mappingConfig ) {
 
+		var conditions = mappingConfig.conditions,
+			i, i_cache,
+			i_len,
+			cloneArray = [];
+
+		if ( !mappingConfig.test ) {
+			return;
+		}
+
+
+		// Get the value to be tested
+		var value = getValue( content, mappingConfig.assess || mappingConfig.value );
+
+
+		// Get the function to use
+		var returnEval = functionForTest[ mappingConfig.test ].call( content, value, mappingConfig.expect );
+
+		// TODO: Run the operand
+
+
+		// If not true, go next
+		if ( !returnEval ) {
+			return false;
+		}
+
+		// Run mapping if satisfied
+		return true;
+	},
+
+	functionForTypedMapping = {
+		"rdf:Alt": function( elm, clone, content, mappingConfig ) {
+
+			var mapping = mappingConfig.mapping,
+				i, i_cache,
+				i_len = mapping.length,
+				canProcess,
+				value = content;
+
+			for ( i = 0; i < i_len || i === 0; i += 1 ) {
+				i_cache = mapping[ i ];
+
+				if ( canProcessMapping( content, i_cache ) ) {
+
+					i_cache = $.extend( true, {}, i_cache ); // Clone the object
+
+					// Remove the test, because it was checked
+					delete i_cache.test;
+
+					// Navigate the content if specified
+					if ( i_cache.value ) {
+						value = getValue( content, i_cache.value );
+					}
+
+
+					// Process the mapping
+					processMapping( elm, clone, value, i_cache );
+
+					// End
+					return;
+				}
+			}
+		}
+	},
 
 	functionForTest = {
 
 		"fn:isArray": function( value ) {
-			GlobalIsArrayTrue = true;
 			return $.isArray( value );
 		},
 
@@ -598,15 +671,17 @@ var componentName = "wb-data-json",
 
 			if ( !guestType ) {
 				if ( typeof value === "string" && value.match( /^([a-z][a-z0-9+\-.]*):/i ) ) {
-					guestType = "xsd:anyURI";
+					guestType = [ "xsd:anyURI", "rdfs:Literal" ];
 				} else if ( typeof value === "string" ) {
-					guestType = "xsd:string";
+					guestType = [ "xsd:string", "rdfs:Literal" ];
 				} else if ( typeof value === "boolean" ) {
-					guestType = "xsd:boolean";
+					guestType = [ "xsd:boolean", "rdfs:Literal" ];
 				} else if ( typeof value === "number" ) {
-					guestType = "xsd:double";
+					guestType = [ "xsd:double", "rdfs:Literal" ];
 				} else if ( typeof value === "undefined" ) {
 					guestType = "undefined";
+				} else if ( typeof value === "null" ) {
+					guestType = "null";
 				} else if ( $.isArray( value ) ) {
 					guestType = "rdfs:Container";
 				} else {
@@ -629,15 +704,22 @@ var componentName = "wb-data-json",
 
 	},
 
+	functionForOperand = {
+		"eq": function( value, expect ) {
+
+		},
+		"neq": function( value, expect ) {
+
+		},
+		"in": function( value, expect ) {
+
+		},
+		"inst": function( value, expect ) {
+
+		}
+	},
+
 	processMapping = function( elm, clone, content, mappingConfig ){
-
-		console.log( "Processing the mapping of....." );
-		console.log( elm );
-		console.log( clone );
-		console.log( content );
-		console.log( mappingConfig );
-		console.log( "END ++++++++++++++++++++Processing the mapping of....." );
-
 
 		var j, j_cache,
 			cached_node, cached_value,
@@ -648,20 +730,66 @@ var componentName = "wb-data-json",
 			upstreamClone, template;
 
 
+		// Is this mapping a special mapping type?
+		if ( mappingConfig[ "@type" ] ) {
+			functionForTypedMapping[ mappingConfig[ "@type" ] ].call( content, elm, clone, content, mappingConfig );
+			return;
+		}
+
+		// Can we proceed?
+		if ( mappingConfig.test && !canProcessMapping( content, mappingConfig ) ) {
+			return;
+		}
+
 		// Check if there is some mapping configuration
 		if ( !mapping && !queryAll ) {
 			return;
 		}
 
+		// Clone mappingConfig to ensure it don't interfere with subsequent data iteration
+		mappingConfig = $.extend( true, {}, mappingConfig );
+
+		// If there is a "template" property, get the inner template
+		if ( mappingConfig.template ) { //&& !mappingConfig.ingnoreTemplate ) {
+			template = clone.querySelector( mappingConfig.template );
+
+			upstreamClone = clone; // Keep reference of the top clone
+
+			//if ( !mappingConfig.tobeclone ) {
+				clone = template.content.cloneNode( true );
+			//} else {
+			//	clone = template.content.querySelector( mappingConfig.tobeclone ).cloneNode( true );
+			//}
+
+			// Ensure we don't recreated it if during a subsequent iteration
+			delete mappingConfig.template;
+		}
+
+
 		// Is content an array?
 		if ( $.isArray( content ) ) {
-			console.log( "THIS IS an array");
 
 			/*if( mappingConfig.template ) {
 				mappingConfig.ingnoreTemplate = true;
 			}*/
 
+			//dataIterator( clone, content, mappingConfig, clone );
 			dataIterator( clone, content, mappingConfig, clone );
+
+			if ( template ) {
+				if ( template.parentNode ) {
+
+
+					//template.parentNode.insertBefore( clone, template );
+					template.parentNode.insertBefore( clone, template );
+					//upstreamClone.appendChild( clone );
+				} else {
+					upstreamClone.appendChild( clone );
+				}
+
+				return elm;
+
+			}
 			return;
 		}
 
@@ -674,34 +802,6 @@ var componentName = "wb-data-json",
 		}
 		mapping_len = mapping.length;
 
-
-		// If there is a "template" property, get the inner template
-		if ( mappingConfig.template ) { //&& !mappingConfig.ingnoreTemplate ) {
-			template = clone.querySelector( mappingConfig.template );
-
-			console.log( "hummmmm" );
-			console.log( clone );
-			console.log( template );
-			console.log( mappingConfig.template );
-
-			upstreamClone = clone; // Keep reference of the top clone
-
-			//if ( !mappingConfig.tobeclone ) {
-				clone = template.content.cloneNode( true );
-			//} else {
-			//	clone = template.content.querySelector( mappingConfig.tobeclone ).cloneNode( true );
-			//}
-
-			console.log( "Override clone" );
-			console.log( clone );
-		}
-
-		if ( mappingConfig.template === "[data-download-link]" ) {
-			console.log( "Specific logging....");
-			console.log( mappingConfig );
-			console.log( upstreamClone );
-			console.log( clone );
-		}
 
 
 
@@ -772,11 +872,7 @@ var componentName = "wb-data-json",
 			//if ( selElements ) {
 			//	cached_node = selElements[ j ];
 			//} else
-			if (! j_cache ) {
-				console.log( content );
-				console.log( mappingConfig );
-				console.log( clone );
-			}
+
 
 			var innerTemplate, outerClone;
 
@@ -813,9 +909,16 @@ var componentName = "wb-data-json",
 			}
 
 
+
 			cached_value = getValue( content, j_cache );
 
 
+
+			/*if ( cached_value && cached_value[ "@id" ] && cached_value[ "@id" ] === "_:sc_1.4.3" ){
+				// for debug
+				console.log( cached_value );
+			}*/
+/*
 			if ( template || !cached_node ) {
 				console.log( "value" );
 				console.log( template );
@@ -824,7 +927,7 @@ var componentName = "wb-data-json",
 				console.log( j_cache );
 				console.log( content );
 
-			}
+			}*/
 
 
 			// Go to the next mapping if the value of JSON node don't exist to ensure we keep the default text set in the template, but move ahead if empty or null
@@ -837,24 +940,20 @@ var componentName = "wb-data-json",
 			// Deep dive into the content if a mapping exist
 			if ( $.isArray( cached_value ) && ( j_cache.mapping || j_cache.queryall ) ) {
 
-				console.log( "calling dataIterator isArray" );
+				/*console.log( "calling dataIterator isArray" );
 				console.log( cached_node );
 				console.log( cached_value );
 				console.log( j_cache );
 				console.log( outerClone );
 				console.log( innerTemplate );
 				console.log( clone );
+				*/
 
-				if ( !outerClone ) {
-					console.log( "no outerClone" );
-					dataIterator( cached_node, cached_value, j_cache );
-				} else {
-					console.log( "with outerClone" );
-					dataIterator( cached_node, cached_value, j_cache,  );
-				}
+				dataIterator( cached_node, cached_value, j_cache );
+
 			} else if ( j_cache.mapping || j_cache.queryall ) {
 
-				console.log( "Deep diving, value is literal" );
+				/*console.log( "Deep diving, value is literal" );
 				console.log( cached_node );
 				console.log( cached_value );
 				console.log( j_cache );
@@ -862,6 +961,7 @@ var componentName = "wb-data-json",
 				console.log( upstreamClone );
 				console.log( innerTemplate );
 				console.log( clone );
+				*/
 
 				//processMapping( upstreamClone, cached_node, cached_value, j_cache );
 				processMapping( template, cached_node, cached_value, j_cache );
@@ -895,7 +995,7 @@ var componentName = "wb-data-json",
 		if ( template ) {
 			if ( template.parentNode ) {
 
-				console.log( "template AJOUTER" );
+				//console.log( "template AJOUTER" );
 
 				//template.parentNode.insertBefore( clone, template );
 				template.parentNode.insertBefore( clone, template );
@@ -915,126 +1015,6 @@ var componentName = "wb-data-json",
 		//return clone;
 	},
 
-	/* work with this setting on the HTML file
-
-
-	<tbody data-wb-json='{
-		"url": "#[a11y]/earl:result",
-		"tobeclone": "tr",
-		"source": "#tmpl-result",
-		"mapping": [
-			{ "selector": "[data-label]", "value": "/earl:test" },
-			{ "selector": "[data-outcome]", "value": "/earl:outcome" },
-			{ "selector": "[data-applicability]", "value": "/earl:subject/dct:description" },
-			{ "selector": "[data-notes]", "value": "/dct:description" },
-
-			{ "selector": "[data-mode]", "value": "/earl:mode" },
-			{ "selector": "[data-severity]", "value": "/acr:severity" },
-			{ "selector": "[data-relevancy]", "value": "/acr:relevancy" },
-			{
-				"selector": "[data-specific-results]",
-				"value": "/earl:result",
-				"mapping": [
-					{ "selector": "[data-result-label]", "value": "/earl:test" },
-					{ "selector": "[data-result-outcome]", "value": "/earl:outcome" },
-					{ "selector": "[data-result-severity]", "value": "/acr:severity" },
-					{ "selector": "[data-result-relevancy]", "value": "/acr:relevancy" },
-					{ "selector": "[data-result-notes", "value": "/dct:description" }
-				]
-			}
-		],
-		"conditions": [
-			{
-				"@type": "rdf:Alt",
-				"conditions": [
-					{
-						"test": "fn:isLiteral",
-						"operand": "is",
-						"value": "/acr:asset",
-						"conditions": [
-							{
-								"@type": "rdf:Alt",
-								"conditions": [
-									{
-										"template": "[data-asset-literal]",
-										"test": "fn:guestType",
-										"operand": "is",
-										"value": "/acr:asset",
-										"expect": "xsd:anyURI",
-										"mapping": [
-											{ "selector": "a", "value": "/acr:asset", "attr": "href" }
-										]
-									},
-									{
-										"template": "[data-asset-code]",
-										"test": "fn:guestType",
-										"operand": "is",
-										"value": "/acr:asset",
-										"expect": "xsd:string",
-										"mapping": [
-											{ "selector": "code", "value": "/acr:asset" }
-										]
-									}
-								]
-							}
-						]
-					},
-					{
-						"template": "[data-asset-image]",
-						"test": "fn:guestType",
-						"operand": "is",
-						"value": "/acr:asset",
-						"expect": "acr:AttachmentImage",
-						"mapping": [
-							{ "selector": "img", "value": "/acr:asset/acr:content/@value", "attr": "src" },
-							{ "selector": "a", "value": "/acr:asset/acr:content/@value", "attr": "href" },
-							{ "selector": "img", "value": "/acr:asset/dct:title", "attr": "alt" }
-
-						]
-					},
-					{
-						"template": "[data-asset-literal]",
-						"test": "fn:guestType",
-						"operand": "is",
-						"value": "/acr:asset",
-						"expect": "xsd:anyURI",
-						"mapping": [
-							{ "selector": "a", "value": "/acr:asset", "attr": "href" }
-						]
-					},
-					{
-						"template": "[data-asset-code]",
-						"test": "fn:guestType",
-						"operand": "is",
-						"value": "/acr:asset",
-						"expect": "xsd:string",
-						"mapping": [
-							{ "selector": "code", "value": "/acr:asset" }
-						]
-					},
-					{
-						"about": "When the value is an array of items",
-						"template": "[data-asset-array]",
-						"test": "fn:isArray",
-						"operand": "is",
-						"value": "/acr:asset",
-						"mapping": [
-								{
-									"template": "[data-download-link]",
-									"value": "/",
-									"mapping": [
-										{ "selector": "a", "value": "/@value", "attr": "href" }
-									]
-
-								}
-						]
-					}
-				]
-			}
-		]
-	}'>
-	</tbody>
-	*/
 
 	getTemplateClone = function( elm, mappingConfig, template ){
 
@@ -1058,20 +1038,21 @@ var componentName = "wb-data-json",
 		return clone;
 	},
 
-
 	getValue = function ( source, pointer ) {
 
 		var value;
 		// var endWithAtValue = value.match( /\/@value$/ ); // See fn:guestType value extrator
 
 
-		// Get the value
-		if ( typeof source === "string" ) {
+		// Get the value if source is string or pointer is pointing to root
+		if ( typeof source === "string" || pointer === "/" || pointer === "/@value" || pointer.value === "/" || pointer.value === "/@value" ) {
 			value = source;
 		} else if ( typeof pointer === "string" ) {
 			value = jsonpointer.get( source, pointer );
-		} else {
+		} else if ( pointer.value ) {
 			value = jsonpointer.get( source, pointer.value );
+		} else {
+			value = source;
 		}
 
 		// for JSON-LD @value support
