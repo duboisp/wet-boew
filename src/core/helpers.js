5fcb59f4d84f1cf9dbfd00c4d80ba62976ea2a2f
Pierre Dubois <duboisp2@gmail.com>
2023-09-28 17:09:20 -0400

    wb-config - Prototype 3

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -7,10 +7,11 @@
  */
 ( function( $, wb ) {
 
-wb.getData = function( element, dataName ) {
+wb.getData = function( element, dataName, parseOptions ) {
 	var elm = !element.jquery ? element : element[ 0 ],
 		dataAttr = elm.getAttribute( "data-" + dataName ),
-		dataObj = {};
+		dataObj = {},
+		configElm, dataConfig;
 
 	if ( dataAttr ) {
 		try {
@@ -18,41 +19,185 @@ wb.getData = function( element, dataName ) {
 			$.data( elm, dataName, dataObj );
 		} catch ( error ) {
 			console.info( elm );
-			$.error( "Bad JSON array in data-" + dataName + " attribute" );
+			console.error( "Bad JSON array in data-" + dataName + " attribute" );
 		}
 	}
 
-	// Check if there is web-component wb-config?
+	// Check if there is web-component wb-config
+	configElm = elm.querySelector( ":scope wb-config[wb-plugin=" + dataName + "]" ) || document.getElementById( elm.getAttribute( "data-" + dataName + "-config" ) );
 
+	if ( configElm ) {
+
+		// If wb-ignore are not define, set it by default to: wb-ignore="id"
+		if ( !configElm.hasAttribute( "wb-ignore" ) ) {
+			configElm.setAttribute( "wb-ignore", "id" );
+		}
+
+		// Get the setting from wb-config element
+		dataConfig = getWbConfig( configElm, parseOptions ).parsed;
+
+		if ( !dataAttr ) {
+			dataObj = dataConfig;
+		} else if ( Array.isArray( dataConfig ) && Array.isArray( dataObj ) ) {
+			dataObj = dataObj.concat( dataConfig );
+		} else if ( !Array.isArray( dataConfig ) && !Array.isArray( dataObj ) ) {
+			dataObj = $.extend( true, {}, dataObj, dataConfig );
+		} else {
+			console.info( elm );
+			console.error( "Incompatible setting, array and object can not be merged" );
+
+			// Let's use the dataConfig
+			dataObj = dataConfig;
+		}
+	}
 
 	return dataObj;
 };
 
-class WbConfig extends HTMLTemplateElement {
-	// <wb-config hello=world>Hello you</wb-config>
-	static observedAttributes = ["disabled"];
+function getWbConfig( elm, parseOptions ) {
 
-	constructor() {
-		super();
+	var context = {},
+		configuration = {},
+		i, i_len = elm.attributes.length,
+		attr, attrName, attrValue,
+		innerConfig = elm.querySelectorAll( ":scope > wb-config" ),
+		innerConfigArray = [],
+		lstIgnoreProp,
+		configParsed,
+		parsedValue;
+
+	// Validate parse Options
+	parseOptions = parseOptions || {};
+
+	// Get all attribute and triage aside the contextual ones prefixed with "wb-"
+	for ( i = 0; i !== i_len; i++ ) {
+		attr = elm.attributes[ i ];
+		attrName = attr.nodeName;
+		attrValue = attr.value;
+
+		// Is it a contextual setting
+		if ( attrName.startsWith( "wb-" ) ) {
+			context[ attrName.substring( 3 ) ] = attrValue;
+			continue;
+		}
+
+		// Exclude any data-* attribute
+		if ( attrName.startsWith( "data-" ) ) {
+			continue;
+		}
+
+		configuration[ attrName ] = parseConfigValue( attrName, attrValue, parseOptions );
 	}
 
-	connectedCallback() {
-
-		console.log( "connectedCallBack" );
-		this.hidden = true; // And also use CSS to hide it.
+	// Is there any property to remove/cleanup?
+	if ( context.ignore ) {
+		lstIgnoreProp = context.ignore.split( " " );
+		i_len = lstIgnoreProp.length;
+		for ( i = 0; i !== i_len; i++ ) {
+			if ( configuration[ lstIgnoreProp[ i ] ] !== undefined ) {
+				delete configuration[ lstIgnoreProp[ i ] ];
+			}
+		}
 	}
 
-	disconnectedCallback() {
-		console.log( "disconnectedCallBack" );
+	// Is there any wb-config children?
+	i_len = innerConfig.length;
+	for ( i = 0; i !== i_len; i++ ) {
+		configParsed = getWbConfig( innerConfig[ i ], parseOptions );
+
+		if ( configParsed.context.prop ) {
+			configuration[ configParsed.context.prop ] = configParsed.parsed;
+		} else {
+			innerConfigArray.push( configParsed.parsed );
+		}
 	}
 
-	attributeChangedCallback(name, oldValue, newValue) {
-		console.log( "attribute change \"" + name + "\" from: " + oldValue + " TO: " + newValue );
-   }
+	// Check if the data type enforced for textContent are JSON
+	if ( context.type === "json" && !context.value ) {
+		context.value = elm.textContent;
+	}
 
+	// Define the true value of the configuration
+	if ( !innerConfig.length && ( context.prop || context.value ) ) {
+
+		parsedValue = {};
+
+		// Let's figure the value
+		if ( context.value ) {
+			try {
+				parsedValue = JSON.parse( context.value );
+			} catch ( error ) {
+				console.info( elm );
+				console.error( "Bad JSON in wb-value attribute" );
+			}
+		} else {
+
+			// Map the text content value
+			if ( context.type === "bool" ) {
+				parsedValue = parseConfigValue( context.prop, elm.textContent, { expectBool: [ context.prop ] } );
+			} else if ( context.type === "number" ) {
+				parsedValue = parseConfigValue( context.prop, elm.textContent, { expectNb: [ context.prop ] } );
+			} else if ( context.type === "pre" ) {
+				parsedValue = parseConfigValue( context.prop, elm.textContent, parseOptions );
+			} else {
+				parsedValue = parseConfigValue( context.prop, elm.textContent.trim(), parseOptions );
+			}
+		}
+
+		// Add the property value as sibling prop
+		if ( Object.keys( configuration ).length ) {
+			if ( !parsedValue === "" || context.type === "pre" ) {
+				configuration[ context.prop ] = parsedValue;
+			}
+		} else {
+			configuration = parsedValue;
+		}
+
+	} else if ( innerConfigArray.length && ( !Object.keys( configuration ).length ) ) {
+
+		// The value are an array of object
+		configuration = innerConfigArray;
+	} else if ( innerConfigArray.length && Object.keys( configuration ).length ) {
+
+		// We can't mix key/value items with anonymous object
+		console.error( elm );
+		console.error( "Error in the configuration, Unnamed property or incompatible config type where array must not be intermixed with key/value.\nThe following config are ignored" );
+		console.error( innerConfigArray );
+	}
+
+	return {
+		context: context,
+		parsed: configuration
+	};
 }
 
-customElements.define("wb-config", WbConfig, { extends: "template" } );
+function parseConfigValue( propName, propValue, options ) {
+
+	var expectedBooleanValue = options.expectBool || [],
+		expectedNumberValue = options.expectNb || [];
+
+	// Adjust the value based on plugin configuration expectation
+	if ( expectedBooleanValue.indexOf( propName ) !== -1 ) {
+		switch ( propValue ) {
+
+		case "false":
+		case "0":
+			propValue = false;
+			break;
+
+		case "true":
+		default:
+			propValue = true;
+			break;
+		}
+	}
+
+	if ( expectedNumberValue.indexOf( propName ) !== -1 ) {
+		propValue = Number.parseFloat( propValue );
+	}
+
+	return propValue;
+}
 
 /*
  * Initiate an in-browser download from a blob
